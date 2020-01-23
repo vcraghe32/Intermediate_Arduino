fc299f23712dce0f3b7ab5f6e9991c92166d7553
Violet K Craghead-Way <vcraghe32@ccs.local>
2020-01-23 15:17:44 -0500

    added motor control and images

---
+++ motor_control_arduino/motor_control_arduino.ino
@@ -0,0 +1,14 @@
+const int potentpin = A0;
+int transispin = A1;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(potentpin, INPUT);
+pinMode(transispin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
