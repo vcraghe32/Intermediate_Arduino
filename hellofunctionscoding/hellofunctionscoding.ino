0c009048704804e99584ba7969475640722478f2
Violet K Craghead-Way <vcraghe32@ccs.local>
2020-02-04 15:37:03 -0500

    added hello functions

---
+++ hellofunctionscoding/hellofunctionscoding.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+const int trigPin = 10;
+const int echoPin = 9;
+int duration;
+int distance;
+Servo myservo;
+void setup()
+{
+ pinMode(echoPin, INPUT);
+ pinMode(trigPin, OUTPUT);
+ Serial.begin(9600);
+ myservo.attach(2);
+}
+void loop()
+{
+ Serial.print("distance:");
+ Serial.println(distance);
+ getDistance();
+ myservo.write(78);
+  if (distance <= 10)
+  {
+   myservo.write(0)
+  }
+ 
+}
+int getDistance()
+{
+ digitalWrite(trigPin, LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = duration * (0.034)/2;
+ return distance;
+}
