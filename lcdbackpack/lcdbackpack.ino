e7465eaca08348a7ab74b9efcfdd78885e3766e1
vcraghe32 <58749842+vcraghe32@users.noreply.github.com>
2020-02-06 15:10:21 -0500

    Update lcdbackpack.ino

--- lcdbackpack/lcdbackpack.ino
+++ lcdbackpack/lcdbackpack.ino
@@ -5,13 +5,13 @@ void setup()
 
 	lcd.init();
 	lcd.backlight();
-	lcd.print("hello, world!");
+	lcd.print("hello, world!"); //print out hello world at the start of the function
 }
 
 void loop()
 {
 	lcd.setCursor(0, 1);
-	lcd.print(millis() / 1000);
+	lcd.print(millis() / 1000);  //prints out the seconds since the beginning of the code
 	delay(100);
 
 }
