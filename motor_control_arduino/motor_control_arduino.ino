d0619b76dca402039b87a31ed5240ad376241148
vcraghe32 <58749842+vcraghe32@users.noreply.github.com>
2020-02-06 15:11:33 -0500

    Update motor_control_arduino.ino

--- motor_control_arduino/motor_control_arduino.ino
+++ motor_control_arduino/motor_control_arduino.ino
@@ -1,14 +1,22 @@
-const int potentpin = A0;
-int transispin = A1;
-
-
-void setup() {
-  // put your setup code here, to run once:
-pinMode(potentpin, INPUT);
-pinMode(transispin, INPUT);
+const int dialpin = A5;
+const int diodepin = A4;
+int value;
+int speed;
+void setup()
+{
+    pinMode(dialpin, INPUT);
+    pinMode(diodepin, OUTPUT);
+    
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+ value = analogRead(dialpin);      //Read and save analog value from potentiometer
+ //speed = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
+  analogWrite(diodepin, value);          //Send PWM value to led
+  delay(100);    
 
+    delay(500);    
 }
+
+  
