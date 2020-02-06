630be3bc4cc8658b2f79209b271934c368177d6b
vcraghe32 <58749842+vcraghe32@users.noreply.github.com>
2020-02-06 15:09:23 -0500

    Update lcd2.ino

--- lcd2/lcd2.ino
+++ lcd2/lcd2.ino
@@ -3,11 +3,11 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup()
 {
 	lcd.begin(16, 2);
-	lcd.print("hello, world!");
+	lcd.print("hello, world!");  //prints hello world to the lcd screen
 }
 
 void loop()
 {
 	lcd.setCursor(0, 1);
-	lcd.print(millis() / 1000);
+	lcd.print(millis() / 1000); //prints the number of seconds since the code started
 }
